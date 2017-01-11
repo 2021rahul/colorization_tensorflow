44e0e601e7d8007119434f63641e6f3f1517fe2c
2021rahul <rahulghosh2021@gmail.com>
2017-01-11 21:22:01 +0530

    Indentation macbook

--- build_model.py
+++ build_model.py
@@ -111,7 +111,7 @@ def distorted_inputs():
 		raise ValueError('Please supply a data_dir')
 	data_dir = os.path.join(FLAGS.data_dir, 'cifar-10-batches-bin')
 	images, labels = cifar10_input.distorted_inputs(data_dir=data_dir,
-	                                              batch_size=FLAGS.batch_size)
+												  batch_size=FLAGS.batch_size)
 	if FLAGS.use_fp16:
 		images = tf.cast(images, tf.float16)
 		labels = tf.cast(labels, tf.float16)
@@ -135,8 +135,8 @@ def inputs(eval_data):
 		raise ValueError('Please supply a data_dir')
 	data_dir = os.path.join(FLAGS.data_dir, 'cifar-10-batches-bin')
 	images, labels = cifar10_input.inputs(eval_data=eval_data,
-					      data_dir=data_dir,
-					      batch_size=FLAGS.batch_size)
+						  data_dir=data_dir,
+						  batch_size=FLAGS.batch_size)
 	if FLAGS.use_fp16:
 		images = tf.cast(images, tf.float16)
 		labels = tf.cast(labels, tf.float16)
@@ -161,9 +161,9 @@ def inference(images):
 	# low_level_conv1
 	with tf.variable_scope('low_level_conv1') as scope:
 		kernel = _variable_with_weight_decay('weights',
-						     shape=[3, 3, 1, 64],
-						     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 1, 64],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(images, kernel, [1, 2, 2, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -173,9 +173,9 @@ def inference(images):
 	# low_level_conv2
 	with tf.variable_scope('low_level_conv2') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 64, 128],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 64, 128],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv1, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -185,9 +185,9 @@ def inference(images):
 	# low_level_conv3
 	with tf.variable_scope('low_level_conv3') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 128, 128],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 128, 128],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv2, kernel, [1, 2, 2, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -197,9 +197,9 @@ def inference(images):
 	# low_level_conv4
 	with tf.variable_scope('low_level_conv4') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 128, 256],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 128, 256],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv3, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [256], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -209,9 +209,9 @@ def inference(images):
 	# low_level_conv5
 	with tf.variable_scope('low_level_conv5') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 256, 256],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 256, 256],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv4, kernel, [1, 2, 2, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [256], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -221,9 +221,9 @@ def inference(images):
 	# low_level_conv6
 	with tf.variable_scope('low_level_conv6') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 256, 512],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 256, 512],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv5, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -233,9 +233,9 @@ def inference(images):
 	# mid_level_conv1
 	with tf.variable_scope('mid_level_conv1') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 512, 512],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 512, 512],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv6, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -245,9 +245,9 @@ def inference(images):
 	# mid_level_conv2
 	with tf.variable_scope('mid_level_conv2') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 512, 256],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 512, 256],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(mid_level_conv1, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [256], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -257,9 +257,9 @@ def inference(images):
 	# global_level_conv1
 	with tf.variable_scope('global_level_conv1') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 512, 512],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 512, 512],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(low_level_conv6, kernel, [1, 2, 2, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -269,9 +269,9 @@ def inference(images):
 	# global_level_conv2
 	with tf.variable_scope('global_level_conv2') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 512, 512],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 512, 512],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(global_level_conv1, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -281,9 +281,9 @@ def inference(images):
 	# global_level_conv3
 	with tf.variable_scope('global_level_conv3') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 512, 512],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 512, 512],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(global_level_conv2, kernel, [1, 2, 2, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -293,9 +293,9 @@ def inference(images):
 	# global_level_conv4
 	with tf.variable_scope('global_level_conv4') as scope:
 		kernel = _variable_with_weight_decay('weights',
-		                                     shape=[3, 3, 512, 512],
-		                                     stddev=5e-2,
-		                                     wd=0.0)
+											 shape=[3, 3, 512, 512],
+											 stddev=5e-2,
+											 wd=0.0)
 		conv = tf.nn.conv2d(global_level_conv3, kernel, [1, 1, 1, 1], padding='VALID')
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.0))
 		pre_activation = tf.nn.bias_add(conv, biases)
@@ -307,9 +307,9 @@ def inference(images):
 		reshape = tf.reshape(global_level_conv4, [FLAGS.batch_size, -1])
 		dim = reshape.get_shape()[1].value
 		weights = _variable_with_weight_decay('weights',
-		                                      shape=[dim, 1024],
-		                                      stddev=0.04,
-		                                      wd=0.004)
+											  shape=[dim, 1024],
+											  stddev=0.04,
+											  wd=0.004)
 		biases = _variable_on_cpu('biases', [1024], tf.constant_initializer(0.1))
 		global_level_FC1 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
 		_activation_summary(global_level_FC1)
@@ -317,9 +317,9 @@ def inference(images):
 	# global_level_FC2
 	with tf.variable_scope('global_level_FC2') as scope:
 		weights = _variable_with_weight_decay('weights',
-						      shape=[1024, 512],
-		                                      stddev=0.04,
-		                                      wd=0.004)
+											  shape=[1024, 512],
+											  stddev=0.04,
+											  wd=0.004)
 		biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.1))
 		global_level_FC2 = tf.nn.relu(tf.matmul(global_level_FC1, weights) + biases, name=scope.name)
 		_activation_summary(global_level_FC2)
@@ -327,9 +327,9 @@ def inference(images):
 	# global_level_FC3
 	with tf.variable_scope('global_level_FC3') as scope:
 		weights = _variable_with_weight_decay('weights',
-						      shape=[512, 256],
-						      stddev=0.04,
-						      wd=0.004)
+											  shape=[512, 256],
+											  stddev=0.04,
+											  wd=0.004)
 		biases = _variable_on_cpu('biases', [256], tf.constant_initializer(0.1))
 		global_level_FC3 = tf.nn.relu(tf.matmul(global_level_FC2, weights) + biases, name=scope.name)
 		_activation_summary(global_level_FC3)
@@ -344,17 +344,17 @@ def loss(logits, labels):
 
   Add summary for "Loss" and "Loss/avg".
   Args:
-    logits: Logits from inference().
-    labels: Labels from distorted_inputs or inputs(). 1-D tensor
-            of shape [batch_size]
+	logits: Logits from inference().
+	labels: Labels from distorted_inputs or inputs(). 1-D tensor
+			of shape [batch_size]
 
   Returns:
-    Loss tensor of type float.
+	Loss tensor of type float.
   """
   # Calculate the average cross entropy loss across the batch.
   labels = tf.cast(labels, tf.int64)
   cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-      logits=logits, labels=labels, name='cross_entropy_per_example')
+	  logits=logits, labels=labels, name='cross_entropy_per_example')
   cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
   tf.add_to_collection('losses', cross_entropy_mean)
 
@@ -370,9 +370,9 @@ def _add_loss_summaries(total_loss):
   visualizing the performance of the network.
 
   Args:
-    total_loss: Total loss from loss().
+	total_loss: Total loss from loss().
   Returns:
-    loss_averages_op: op for generating moving averages of losses.
+	loss_averages_op: op for generating moving averages of losses.
   """
   # Compute the moving average of all individual losses and the total loss.
   loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
@@ -382,10 +382,10 @@ def _add_loss_summaries(total_loss):
   # Attach a scalar summary to all individual losses and the total loss; do the
   # same for the averaged version of the losses.
   for l in losses + [total_loss]:
-    # Name each loss as '(raw)' and name the moving average version of the loss
-    # as the original loss name.
-    tf.contrib.deprecated.scalar_summary(l.op.name + ' (raw)', l)
-    tf.contrib.deprecated.scalar_summary(l.op.name, loss_averages.average(l))
+	# Name each loss as '(raw)' and name the moving average version of the loss
+	# as the original loss name.
+	tf.contrib.deprecated.scalar_summary(l.op.name + ' (raw)', l)
+	tf.contrib.deprecated.scalar_summary(l.op.name, loss_averages.average(l))
 
   return loss_averages_op
 
@@ -397,11 +397,11 @@ def train(total_loss, global_step):
   average for all trainable variables.
 
   Args:
-    total_loss: Total loss from loss().
-    global_step: Integer Variable counting the number of training steps
-      processed.
+	total_loss: Total loss from loss().
+	global_step: Integer Variable counting the number of training steps
+	  processed.
   Returns:
-    train_op: op for training.
+	train_op: op for training.
   """
   # Variables that affect learning rate.
   num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
@@ -409,10 +409,10 @@ def train(total_loss, global_step):
 
   # Decay the learning rate exponentially based on the number of steps.
   lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
-                                  global_step,
-                                  decay_steps,
-                                  LEARNING_RATE_DECAY_FACTOR,
-                                  staircase=True)
+								  global_step,
+								  decay_steps,
+								  LEARNING_RATE_DECAY_FACTOR,
+								  staircase=True)
   tf.contrib.deprecated.scalar_summary('learning_rate', lr)
 
   # Generate moving averages of all losses and associated summaries.
@@ -420,28 +420,28 @@ def train(total_loss, global_step):
 
   # Compute gradients.
   with tf.control_dependencies([loss_averages_op]):
-    opt = tf.train.GradientDescentOptimizer(lr)
-    grads = opt.compute_gradients(total_loss)
+	opt = tf.train.GradientDescentOptimizer(lr)
+	grads = opt.compute_gradients(total_loss)
 
   # Apply gradients.
   apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
 
   # Add histograms for trainable variables.
   for var in tf.trainable_variables():
-    tf.contrib.deprecated.histogram_summary(var.op.name, var)
+	tf.contrib.deprecated.histogram_summary(var.op.name, var)
 
   # Add histograms for gradients.
   for grad, var in grads:
-    if grad is not None:
-      tf.contrib.deprecated.histogram_summary(var.op.name + '/gradients', grad)
+	if grad is not None:
+	  tf.contrib.deprecated.histogram_summary(var.op.name + '/gradients', grad)
 
   # Track the moving averages of all trainable variables.
   variable_averages = tf.train.ExponentialMovingAverage(
-      MOVING_AVERAGE_DECAY, global_step)
+	  MOVING_AVERAGE_DECAY, global_step)
   variables_averages_op = variable_averages.apply(tf.trainable_variables())
 
   with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
-    train_op = tf.no_op(name='train')
+	train_op = tf.no_op(name='train')
 
   return train_op
 
@@ -450,17 +450,17 @@ def maybe_download_and_extract():
   """Download and extract the tarball from Alex's website."""
   dest_directory = FLAGS.data_dir
   if not os.path.exists(dest_directory):
-    os.makedirs(dest_directory)
+	os.makedirs(dest_directory)
   filename = DATA_URL.split('/')[-1]
   filepath = os.path.join(dest_directory, filename)
   if not os.path.exists(filepath):
-    def _progress(count, block_size, total_size):
-      sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename,
-          float(count * block_size) / float(total_size) * 100.0))
-      sys.stdout.flush()
-    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
-    print()
-    statinfo = os.stat(filepath)
-    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+	def _progress(count, block_size, total_size):
+	  sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename,
+		  float(count * block_size) / float(total_size) * 100.0))
+	  sys.stdout.flush()
+	filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+	print()
+	statinfo = os.stat(filepath)
+	print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
 
   tarfile.open(filepath, 'r:gz').extractall(dest_directory)
