caaa6c8ab8f81f01f8f157ea117c4f2cb5957683
2021rahul <rahulghosh2021@gmail.com>
2017-03-28 22:23:16 +0530

    input read

--- input_create.py
+++ input_create.py
@@ -1,9 +1,19 @@
 import cv2
 
-img = cv2.imread('img/Architecture.jpg',3)
-greyimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-greyimg = cv2.cvtColor(img,cv2.COLOR_BGR2LAB)
-cv2.imshow('image',greyimg)
+def create_input(filename):
+	img = cv2.imread(filename,3)
+	height, width, channels = img.shape
+	greyimg = cv2.cvtColor(cv2.resize(img, (224, 224)),cv2.COLOR_BGR2GRAY)
+	colorimg = cv2.cvtColor(cv2.resize(img, (224, 224)),cv2.COLOR_BGR2LAB)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	resized_greyimg = cv2.resize(greyimg, (width,height))
+	resized_colorimg = cv2.resize(colorimg, (width,height))
+
+	cv2.imshow('greyimage',greyimg)
+	cv2.imshow('colorimage',colorimg)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+	filename = 'img/Architecture.jpg'
+	create_input(filename)
